256c4a34bc2d2a6bcd7bff903fabff5a99bf4fe6
irsyad <1rsy4d.m@gmail.com>
2020-08-11 23:35:32 +0700

    Added a function that remove Tulupan after the player acquired it

--- World/Tulupan.gd
+++ World/Tulupan.gd
@@ -2,6 +2,10 @@ extends ConsumableItem
 
 signal item_changed(value)
 
+func _ready():
+	if(Global.item == 1):
+		queue_free()
+
 func interaction_interact(interactionComponentParent : Node) -> void:
 	print("Tulupan sudah diambil!")
 	Global.item += 1
