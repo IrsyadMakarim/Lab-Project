7d5c7a6ab83c4c8c741c73a9255b90cd63a199dc
irsyad <1rsy4d.m@gmail.com>
2020-08-13 18:10:58 +0700

    Removed unnecessary files

--- Level/AreaUntukNyalainLevel2.gd
+++
@@ -1,4 +0,0 @@
-extends Area2D
-
-func _on_AreaUntukNyalainLevel2_body_entered(body):
-	Global.level2 = true
--- Level/AreaUntukNyalainPasar.gd
+++
@@ -1,5 +0,0 @@
-extends Area2D
-
-func _on_AreaUntukNyalainPasar_body_entered(body):
-	Global.pasar = true
-	print(Global.pasar)
