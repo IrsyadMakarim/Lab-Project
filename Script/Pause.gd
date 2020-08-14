ae25d1f7da01cf6cd6721bc86c6b20be28b7a8d1
irsyad <1rsy4d.m@gmail.com>
2020-08-14 17:26:10 +0700

    Added a Pause Screen

---
+++ Script/Pause.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _ready():
+	$PauseMenu/VBoxContainer/ItemButton.grab_focus()
+	for btn in $PauseMenu/VBoxContainer.get_children():
+		btn.connect("pressed", self, "_on_Button_pressed", [btn.scene_load])
+
+func _on_Button_pressed(scene_load):
+	get_tree().change_scene(scene_load)
+	get_tree().paused = false
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		var new_pause_state = not get_tree().paused
+		get_tree().paused = new_pause_state
+		visible = new_pause_state
