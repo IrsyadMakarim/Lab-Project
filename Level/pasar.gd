fbd746c6e9204ed1fc91ab9d5de0dbfb8740f39e
irsyad <1rsy4d.m@gmail.com>
2020-08-23 15:43:20 +0700

    Fixes some bugs

--- Level/pasar.gd
+++ Level/pasar.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
 func _on_WorldArea_body_exited(body):
-	Global.pos2 = Vector2(21, 63)
-	get_tree().change_scene("res://Level/World.tscn")
+	Global.pos2 = Vector2(7, 76)
+	get_tree().change_scene("res://Level/perumahan.tscn")
